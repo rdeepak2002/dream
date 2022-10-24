0b8dd06c51f4549076b614ca59f1f96b4d344317
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-10-24 03:51:07 -0400

    Update SDL2 GL minor attribute

--- dream/src/SDLWindow.cpp
+++ dream/src/SDLWindow.cpp
@@ -15,7 +15,7 @@ SDLWindow::SDLWindow(int windowWidth, int windowHeight) {
     this->Window = SDL_CreateWindow("OpenGL Test", 0, 0, windowWidth, windowHeight, WindowFlags);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); // TODO: set this to compatability for web build
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GLContext Context = SDL_GL_CreateContext(Window);
 
     // Check OpenGL properties
