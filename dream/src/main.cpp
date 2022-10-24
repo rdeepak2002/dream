dad7c4da4b20363fcea05e73d0a56830f781c3de
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-10-24 02:38:59 -0400

    Output executable version of engine core to new folder

--- dream/src/main.cpp
+++ dream/src/main.cpp
@@ -2,32 +2,27 @@
 #include <iostream>
 
 #ifdef EMSCRIPTEN
-    #include "emscripten.h"
+#include "emscripten.h"
 #endif
 
-int update()
-{
+void mainLoop() {
+    #ifdef EMSCRIPTEN
+    update();
+    #else
     auto* application = new Application();
 
     while (!application->shouldClose()) {
         application->update();
     }
-
-    return EXIT_SUCCESS;
-}
-
-void mainLoop() {
-    update();
+    #endif
 }
 
 void startUpdateLoop() {
-#ifdef EMSCRIPTEN // TODO: only render on repaint call
-    // Define a mail loop function, that will be called as fast as possible
+    #ifdef EMSCRIPTEN // TODO: only render on repaint call
     emscripten_set_main_loop(&mainLoop, 0, 1);
-#else
-    // This is the normal C/C++ main loop
-    update();
-#endif
+    #else
+    mainLoop();
+    #endif
 }
 
 int main(int ArgCount, char **Args)
