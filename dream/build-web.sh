63510e710a8e85b787320b07616aeba3ea1ed077
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-10 17:59:21 -0500

    Second part of fixing web build for model loading

--- dream/build-web.sh
+++ dream/build-web.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # steps to build assimp:
-# 1. check if assimp already built
-# 2. ...
-
-# TODO: return back to dream/dream directory
+# 1. check if build.web/assimp-5.2.5 folder exists (not tar.gz file) -> if not, then continue, otherwise skip next steps
+# 2. cd build.web
+# 3. extract assimp-5.2.5.tar.gz to assimp-5.2.5 folder
+# 4. cd assimp-5.2.5
+# 5. cmake . -DCMAKE_TOOLCHAIN_FILE=../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Release
+# 6. make
+# 7. cd ..
+# 8. cd ..
 
 rm -rf cmake-build-debug-web
 mkdir cmake-build-debug-web
