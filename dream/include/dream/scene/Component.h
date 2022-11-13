b74a606b1f032d998ec07652aba8c1e292b47eaa
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-13 16:28:40 -0500

    Move component method struct implementations to cpp source files

--- dream/include/dream/scene/Component.h
+++ dream/include/dream/scene/Component.h
@@ -9,18 +9,16 @@
 #include <utility>
 #include "dream/scene/Entity.h"
 #include "dream/Project.h"
+#include "dream/renderer/Mesh.h"
 
 namespace Dream::Component {
     struct RootComponent {
         std::string name = "root";
     };
 
-
     struct TagComponent {
         std::string tag = "";
-        explicit TagComponent(std::string tag) {
-            this->tag = std::move(tag);
-        }
+        explicit TagComponent(std::string tag);
     };
 
     struct HierarchyComponent {
@@ -28,71 +26,14 @@ namespace Dream::Component {
         Entity prev {entt::null, nullptr};
         Entity next {entt::null, nullptr};
         Entity parent {entt::null, nullptr};
-        void addChild(Entity &newChild, Entity &newParent) {
-            if (newParent) {
-                // remove child from old parent
-                Entity oldParent = newChild.getComponent<HierarchyComponent>().parent;
-                if (oldParent) {
-                    oldParent.getComponent<HierarchyComponent>().removeChild(newChild);
-                }
-                // add new child as child of parent
-                auto &parentHierarchyComp = newParent.getComponent<HierarchyComponent>();
-                if (parentHierarchyComp.first) {
-                    // insert child into front of list
-                    auto& formerFirstChild = parentHierarchyComp.first;
-                    formerFirstChild.getComponent<HierarchyComponent>().prev = newChild;
-                    newChild.getComponent<HierarchyComponent>().next = formerFirstChild;
-                    newChild.getComponent<HierarchyComponent>().prev = Entity {entt::null, nullptr};
-                    parentHierarchyComp.first = newChild;
-                } else {
-                    // make child head of list
-                    newChild.getComponent<HierarchyComponent>().prev = Entity {entt::null, nullptr};
-                    newChild.getComponent<HierarchyComponent>().next = Entity {entt::null, nullptr};
-                    parentHierarchyComp.first = newChild;
-                }
-                // update parent of new child
-                newChild.getComponent<HierarchyComponent>().parent = newParent;
-            } else {
-                std::cout << "cannot add child to non-existing parent" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        void removeChild(Entity &childToRemove) {
-            if (!childToRemove) {
-                std::cout << "cannot remove null child" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-
-            if (first == childToRemove) {
-                // case where child to remove is head of list
-                first = childToRemove.getComponent<HierarchyComponent>().next;
-            } else {
-                auto &prevEntity = childToRemove.getComponent<HierarchyComponent>().prev;
-                auto &nextEntity = childToRemove.getComponent<HierarchyComponent>().next;
-                if (prevEntity) {
-                    prevEntity.getComponent<HierarchyComponent>().next = nextEntity;
-                }
-                if (nextEntity) {
-                    nextEntity.getComponent<HierarchyComponent>().prev = prevEntity;
-                }
-            }
-        }
-        int numChildren() {
-            int count = 0;
-            Entity c = first;
-            while (c) {
-                count += 1;
-                c = c.getComponent<HierarchyComponent>().next;
-            }
-            return count;
-        }
+        void addChild(Entity &newChild, Entity &newParent);
+        void removeChild(Entity &childToRemove);
+        int numChildren();
     };
 
     struct MeshComponent {
-        std::string name;
-        explicit MeshComponent(std::string name) {
-            this->name = std::move(name);
-        }
+        Mesh* mesh = nullptr;
+        explicit MeshComponent(Mesh* meshIn);
     };
 }
 
--- dream/src/scene/Component.cpp
+++ dream/src/scene/Component.cpp
@@ -3,3 +3,76 @@
 //
 
 #include "dream/scene/Component.h"
+
+namespace Dream {
+    namespace Component {
+        int HierarchyComponent::numChildren() {
+            int count = 0;
+            Entity c = first;
+            while (c) {
+                count += 1;
+                c = c.getComponent<HierarchyComponent>().next;
+            }
+            return count;
+        }
+
+        void HierarchyComponent::removeChild(Entity &childToRemove) {
+            if (!childToRemove) {
+                std::cout << "cannot remove null child" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+            if (first == childToRemove) {
+                // case where child to remove is head of list
+                first = childToRemove.getComponent<HierarchyComponent>().next;
+            } else {
+                auto &prevEntity = childToRemove.getComponent<HierarchyComponent>().prev;
+                auto &nextEntity = childToRemove.getComponent<HierarchyComponent>().next;
+                if (prevEntity) {
+                    prevEntity.getComponent<HierarchyComponent>().next = nextEntity;
+                }
+                if (nextEntity) {
+                    nextEntity.getComponent<HierarchyComponent>().prev = prevEntity;
+                }
+            }
+        }
+
+        void HierarchyComponent::addChild(Entity &newChild, Entity &newParent) {
+            if (newParent) {
+                // remove child from old parent
+                Entity oldParent = newChild.getComponent<HierarchyComponent>().parent;
+                if (oldParent) {
+                    oldParent.getComponent<HierarchyComponent>().removeChild(newChild);
+                }
+                // add new child as child of parent
+                auto &parentHierarchyComp = newParent.getComponent<HierarchyComponent>();
+                if (parentHierarchyComp.first) {
+                    // insert child into front of list
+                    auto& formerFirstChild = parentHierarchyComp.first;
+                    formerFirstChild.getComponent<HierarchyComponent>().prev = newChild;
+                    newChild.getComponent<HierarchyComponent>().next = formerFirstChild;
+                    newChild.getComponent<HierarchyComponent>().prev = Entity {entt::null, nullptr};
+                    parentHierarchyComp.first = newChild;
+                } else {
+                    // make child head of list
+                    newChild.getComponent<HierarchyComponent>().prev = Entity {entt::null, nullptr};
+                    newChild.getComponent<HierarchyComponent>().next = Entity {entt::null, nullptr};
+                    parentHierarchyComp.first = newChild;
+                }
+                // update parent of new child
+                newChild.getComponent<HierarchyComponent>().parent = newParent;
+            } else {
+                std::cout << "cannot add child to non-existing parent" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        MeshComponent::MeshComponent(Mesh* meshIn) {
+            this->mesh = meshIn;
+        }
+
+        TagComponent::TagComponent(std::string tag) {
+            this->tag = std::move(tag);
+        }
+    }
+}
