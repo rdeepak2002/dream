27d44cb5736e9c9abc5662689ced3e31b6c891a3
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-19 21:18:40 -0500

    Move YAML utility conversion methods to YAMLUtils

--- dream/include/dream/scene/component/Component.h
+++ dream/include/dream/scene/component/Component.h
@@ -13,6 +13,7 @@
 #include "dream/scene/Entity.h"
 #include "dream/renderer/Mesh.h"
 #include "dream/renderer/Texture.h"
+#include "dream/util/YAMLUtils.h"
 
 namespace Dream::Component {
     struct Component {
@@ -123,110 +124,6 @@ namespace Dream::Component {
         void loadTexture();
         static void deserialize(YAML::Node node, Entity &entity);
         static void serialize(YAML::Emitter &out, Entity &entity);
-    };
-}
-
-namespace YAML {
-    template<>
-    struct convert<glm::vec2>
-    {
-        static Node encode(const glm::vec2& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
-        static bool decode(const Node& node, glm::vec2& rhs)
-        {
-            if (!node.IsSequence() || node.size() != 2)
-                return false;
-
-            rhs.x = node[0].as<float>();
-            rhs.y = node[1].as<float>();
-            return true;
-        }
-    };
-
-    template<>
-    struct convert<glm::vec3>
-    {
-        static Node encode(const glm::vec3& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.push_back(rhs.z);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
-        static bool decode(const Node& node, glm::vec3& rhs)
-        {
-            if (!node.IsSequence() || node.size() != 3)
-                return false;
-
-            rhs.x = node[0].as<float>();
-            rhs.y = node[1].as<float>();
-            rhs.z = node[2].as<float>();
-            return true;
-        }
-    };
-
-    template<>
-    struct convert<glm::vec4>
-    {
-        static Node encode(const glm::vec4& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.push_back(rhs.z);
-            node.push_back(rhs.w);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
-        static bool decode(const Node& node, glm::vec4& rhs)
-        {
-            if (!node.IsSequence() || node.size() != 4)
-                return false;
-
-            rhs.x = node[0].as<float>();
-            rhs.y = node[1].as<float>();
-            rhs.z = node[2].as<float>();
-            rhs.w = node[3].as<float>();
-            return true;
-        }
-    };
-
-    template<>
-    struct convert<glm::quat>
-    {
-        static Node encode(const glm::quat& rhs)
-        {
-            Node node;
-            node.push_back(rhs.w);
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.push_back(rhs.z);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
-        static bool decode(const Node& node, glm::quat& rhs)
-        {
-            if (!node.IsSequence() || node.size() != 4)
-                return false;
-
-            rhs.w = node[0].as<float>();
-            rhs.x = node[1].as<float>();
-            rhs.y = node[2].as<float>();
-            rhs.z = node[3].as<float>();
-            return true;
-        }
     };
 }
 
---
+++ dream/include/dream/util/YAMLUtils.h
@@ -0,0 +1,43 @@
+//
+// Created by Deepak Ramalingam on 11/19/22.
+//
+
+#ifndef DREAM_YAMLUTILS_H
+#define DREAM_YAMLUTILS_H
+
+#include <iostream>
+#include <utility>
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <yaml-cpp/yaml.h>
+#include "dream/scene/Entity.h"
+#include "dream/renderer/Mesh.h"
+#include "dream/renderer/Texture.h"
+
+namespace YAML {
+    template<>
+    struct convert<glm::vec2> {
+        static Node encode(const glm::vec2& rhs);
+        static bool decode(const Node& node, glm::vec2& rhs);
+    };
+
+    template<>
+    struct convert<glm::vec3> {
+        static Node encode(const glm::vec3& rhs);
+        static bool decode(const Node& node, glm::vec3& rhs);
+    };
+
+    template<>
+    struct convert<glm::vec4> {
+        static Node encode(const glm::vec4& rhs);
+        static bool decode(const Node& node, glm::vec4& rhs);
+    };
+
+    template<>
+    struct convert<glm::quat> {
+        static Node encode(const glm::quat& rhs);
+        static bool decode(const Node& node, glm::quat& rhs);
+    };
+}
+
+#endif //DREAM_YAMLUTILS_H
