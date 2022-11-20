56bcdb731ddfd1f8d0a6ca5dd3eb0abd36d893be
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-19 19:27:43 -0500

    Remove commented out example scene loading code in Application

--- dream/src/Application.cpp
+++ dream/src/Application.cpp
@@ -7,8 +7,6 @@
 #include "dream/renderer/OpenGLRenderer.h"
 #include "dream/editor/ImGuiSDL2OpenGLEditor.h"
 #include "dream/window/SDL2OpenGLWindow.h"
-#include "dream/scene/component/Component.h"
-#include <iostream>
 #include <filesystem>
 #include <map>
 
@@ -18,65 +16,6 @@ namespace Dream {
         this->window = new SDL2OpenGLWindow();
         this->renderer = new OpenGLRenderer();
         this->editor = new ImGuiSDL2OpenGLEditor(this->window);
-
-        // TODO: load from project's scene file
-//        auto sphereEntity = Project::getScene()->createEntity("Sphere");
-//        std::map<std::string, float> sphereData;
-//        sphereEntity.addComponent<Component::MeshComponent>(Component::MeshComponent::MeshType::PRIMITIVE_SPHERE, sphereData);
-//        sphereEntity.getComponent<Component::TransformComponent>().translation = {-0.7, -0.7, 0};
-//        sphereEntity.getComponent<Component::TransformComponent>().scale = {0.4, 0.4, 0.4};
-//
-//        auto cubeEntity = Project::getScene()->createEntity("Cube");
-//        std::map<std::string, float> cubeData;
-//        cubeEntity.addComponent<Component::MeshComponent>(Component::MeshComponent::MeshType::PRIMITIVE_CUBE, cubeData);
-//        cubeEntity.addComponent<Component::MaterialComponent>("8C48D481-C949-4C24-87CF-05E446A3Y8A2");
-//        cubeEntity.getComponent<Component::TransformComponent>().translation = {1, 0.7, 0};
-//        cubeEntity.getComponent<Component::TransformComponent>().scale = {0.4, 0.4, 0.4};
-
-//        Entity teapotEntity = Project::getAssetImporter()->importMesh(Project::getPath().append("assets").append("models").append("teapot.stl"));
-//        teapotEntity.getComponent<Component::TransformComponent>().scale = {0.05, 0.05, 0.05};
-
-//        Entity cuteGhostEntity = Project::getAssetImporter()->importMesh(Project::getPath().append("assets").append("models").append("cute-ghost").append("source").append("Ghost.fbx"));
-//        if (cuteGhostEntity) {
-//            cuteGhostEntity.getComponent<Component::TransformComponent>().translation = {0, 0, 0};
-//            cuteGhostEntity.getComponent<Component::TransformComponent>().scale = {0.2, 0.2, 0.2};
-//            cuteGhostEntity.getComponent<Component::TransformComponent>().rotation = {0.707, -0.5, 0, 0.0};
-//        } else {
-//            std::cout << "Error importing ghost model" << std::endl;
-//        }
-
-//        Entity knightEntity = Project::getAssetLoader()->loadMesh(Project::getPath().append("assets").append("models").append("knight").append("scene.gltf"));
-//        if (knightEntity) {
-//            knightEntity.getComponent<Component::TransformComponent>().translation = {0, -0.3, 0};
-//            knightEntity.getComponent<Component::TransformComponent>().scale = {0.007, 0.007, 0.007};
-//        } else {
-//            std::cout << "Error importing knight model" << std::endl;
-//        }
-
-//        Entity dragonEntity = Project::getAssetLoader()->loadMesh("7F555714-27E0-4AB5-B950-3BD20ED6E914", true); // guid comes from alduin/scene.gltf.meta
-//        if (dragonEntity) {
-//            dragonEntity.getComponent<Component::TransformComponent>().translation = {0.5, 0, 0};
-//            dragonEntity.getComponent<Component::TransformComponent>().scale = {0.0025, 0.0025, 0.0025};
-//            dragonEntity.getComponent<Component::TransformComponent>().rotation = {0.707, 0.707, 0, 0};
-//        } else {
-//            std::cout << "Error importing dragon model" << std::endl;
-//        }
-
-//        Entity sponzaEntity = Project::getAssetLoader()->loadMesh(Project::getPath().append("assets").append("models").append("sponza").append("Sponza.gltf"));
-//        if (sponzaEntity) {
-//            sponzaEntity.getComponent<Component::TransformComponent>().translation = {0.0, 0, 0};
-//            sponzaEntity.getComponent<Component::TransformComponent>().scale = {1, 1, 1};
-//            sponzaEntity.getComponent<Component::TransformComponent>().rotation = {0.707, 0.707, 0, 0};
-////            sponzaEntity.getComponent<Component::TransformComponent>().translation = {0.0, -2, 2.0};
-////            sponzaEntity.getComponent<Component::TransformComponent>().scale = {0.0025, 0.0025, 0.0025};
-////            sponzaEntity.getComponent<Component::TransformComponent>().rotation = {0, 0, 0, 1.0};
-//        } else {
-//            std::cout << "Error importing sponza model" << std::endl;
-//        }
-
-//        Project::getAssetImporter()->importAsset("/Users/deepakramalingam/Desktop/sponza");
-
-//        Project::saveScene();
     }
 
     Application::~Application() {
