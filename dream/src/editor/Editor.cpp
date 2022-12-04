65e824a908712fcce22c77413e6fa411077d65c4
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-12-04 17:30:43 -0500

    Refactor namespace encapsulation of Editor class

--- dream/src/editor/Editor.cpp
+++ dream/src/editor/Editor.cpp
@@ -4,22 +4,24 @@
 
 #include "dream/editor/Editor.h"
 
-void Dream::Editor::update(Dream::Window *window, unsigned int frameBufferTexture) {
+namespace Dream {
+    void Editor::update(Dream::Window *window, unsigned int frameBufferTexture) {
 
-}
+    }
 
-std::pair<int, int> Dream::Editor::getRendererViewportDimensions() {
-    return std::pair<int, int>(0, 0);
-}
+    std::pair<int, int> Editor::getRendererViewportDimensions() {
+        return std::pair<int, int>(0, 0);
+    }
 
-Dream::Editor::Editor(Dream::Window *window) {
+    Editor::Editor(Dream::Window *window) {
 
-}
+    }
 
-void Dream::Editor::newFrame(Dream::Window *window) {
+    void Editor::newFrame(Dream::Window *window) {
 
-}
+    }
 
-void Dream::Editor::renderDrawData() {
+    void Editor::renderDrawData() {
 
+    }
 }
