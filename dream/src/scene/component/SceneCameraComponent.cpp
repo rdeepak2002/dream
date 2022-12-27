1453fbb6f79509cd23adbdca05289da608b9cb30
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-12-27 14:23:02 -0500

    Add license and copyright preamble to source code files

--- dream/src/scene/component/SceneCameraComponent.cpp
+++ dream/src/scene/component/SceneCameraComponent.cpp
@@ -1,6 +1,20 @@
-//
-// Created by Deepak Ramalingam on 12/10/22.
-//
+/**********************************************************************************
+ *  Dream is a software for developing real-time 3D experiences.
+ *  Copyright (C) 2023 Deepak Ramalignam
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **********************************************************************************/
 
 #include "dream/scene/component/Component.h"
 #include "dream/util/Logger.h"
