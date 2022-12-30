0b815141f83de1403aeca0ea1ad4dde2c293737b
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-12-29 19:24:21 -0500

    Add backwards walking to example scene

--- examples/sample-project/assets/scripts/script0.lua
+++ examples/sample-project/assets/scripts/script0.lua
@@ -1,16 +1,32 @@
 function update(entity, dt)
-	local vel = 0
+	local animVel = 0
 	local slash = 0
+	local xzVel = vec2:new(0, 0)
+
 	if Input.getButtonDown(Key.w) then
-		vel = 1
+		xzVel.y = xzVel.y + 1
+		animVel = 1
 	end
+
+	if Input.getButtonDown(Key.s) then
+		xzVel.y = xzVel.y - 1
+		animVel = -1
+	end
+
 	if Input.getButtonDown(Key.LeftMouse) then
 		slash = 1
 	end
 
-	entity:getAnimator():setVariable("velocity", vel)
+	entity:getRigidBody():setLinearVelocity(vec3:new(xzVel.x, 0, xzVel.y))
+	entity:getAnimator():setVariable("velocity", animVel)
 	entity:getAnimator():setVariable("slash", slash)
 end
+
+
+
+
+
+
 
 
 
