51d8f24e31e78b20f86455198c28135612ce4ee1
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-12-30 22:25:35 -0500

    More typical third person orbit controls

--- examples/sample-project/assets/scripts/script0.lua
+++ examples/sample-project/assets/scripts/script0.lua
@@ -18,11 +18,16 @@ function update(entity, dt)
 	cameraRightVector.y = 0
 	cameraRightVector = MathUtils.normalizeVec3(cameraRightVector)
 
+	-- get forward and right vectors
+	local knightForwardVector = entity:getTransform():getFront()
+	knightForwardVector.y = 0
+	knightForwardVector = MathUtils.normalizeVec3(knightForwardVector)
+	local knightLeftVector = entity:getTransform():getLeft()
+	knightLeftVector.y = 0
+	knightLeftVector = MathUtils.normalizeVec3(knightLeftVector)
+
 	local pKnightTranslation = vec3:new(knightTranslation.x, 0, knightTranslation.z)
 	local pCameraTranslation = vec3:new(cameraTranslation.x, 0, cameraTranslation.z)
-	local targetRotationQuat = MathUtils.safeQuatLookAt(pKnightTranslation, pCameraTranslation, vec3:new(0, 1, 0) , vec3:new(0, 1, 0))
-	-- entity:getTransform().rotation = targetRotationQuat
-	-- entity:getRigidBody():setRotation(targetRotationQuat)
 
 	-- calculate ground linear velocity based off inputs
 	local linearVelocity = vec3:new(0, 0, 0)
@@ -30,12 +35,10 @@ function update(entity, dt)
 	
 	if Input.getButtonDown(Key.w) then
 		linearVelocity = linearVelocity + cameraForwardVector
-		animVelForward = animVelForward + 1
 	end
 
 	if Input.getButtonDown(Key.s) then
 		linearVelocity = linearVelocity - cameraForwardVector
-		animVelForward = animVelForward - 1
 	end
 
 	if Input.getButtonDown(Key.a) then
@@ -47,21 +50,21 @@ function update(entity, dt)
 	end
 
 	if MathUtils.magnitudeVec3(linearVelocity) > 0 then
+		-- set rigid body velocity
 		linearVelocity = MathUtils.normalizeVec3(linearVelocity)
 		linearVelocity = linearVelocity * speed
-		-- entity:getRigidBody():setRotation(targetRotationQuat)
 		entity:getRigidBody():setLinearVelocity(linearVelocity)
+
+		-- rotate rigid body in direction of velocity
+		local targetRotationQuat = MathUtils.safeQuatLookAt(pKnightTranslation, pKnightTranslation - linearVelocity, vec3:new(0, 1, 0) , vec3:new(0, 1, 0))
 		entity:getRigidBody():setRotation(targetRotationQuat)
+
+		-- set velocity for animator
+		entity:getAnimator():setVariable("velocity", 1)
 	else
-		-- entity:getRigidBody():setRotation(vec3:new(0, 0, 0))
-		-- entity:getRigidBody():setLinearVelocity(vec3:new(0, 0, 0))
+		-- set velocity for animator
+		entity:getAnimator():setVariable("velocity", 0)
 	end
-
-	-- entity:getRigidBody():setRotation(quat:new(1, 0, 0, 0))
-	-- entity:getRigidBody():setRotation(targetRotationQuat)
-
-	-- set velocity for animator
-	entity:getAnimator():setVariable("velocity", animVelForward)
 
 	-- set slashing for animator
 	local slash = 0
@@ -69,31 +72,8 @@ function update(entity, dt)
 		slash = 1
 	end
 	entity:getAnimator():setVariable("slash", slash)
-
-	-- entity:getRigidBody():setAngularVelocity(vec3:new(0, 0, 0))
-
-	-- local animVel = 0
-	-- local slash = 0
-	-- local xzVel = vec2:new(0, 0)
-
-	-- if Input.getButtonDown(Key.w) then
-	-- 	xzVel.y = xzVel.y + 1
-	-- 	animVel = 1
-	-- end
-
-	-- if Input.getButtonDown(Key.s) then
-	-- 	xzVel.y = xzVel.y - 1
-	-- 	animVel = -1
-	-- end
-
-	-- if Input.getButtonDown(Key.LeftMouse) then
-	-- 	slash = 1
-	-- end
-
-	-- entity:getRigidBody():setLinearVelocity(vec3:new(xzVel.x, 0, xzVel.y))
-	-- entity:getAnimator():setVariable("velocity", animVel)
-	-- entity:getAnimator():setVariable("slash", slash)
 end
+
 
 
 
