b5d7d7387a9ff7954222f265c884c3cd0343e15c
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-01-17 21:59:26 -0500

    Fix shadows repeating weirdly on WebGL build

--- dream/src/renderer/OpenGLRenderer.cpp
+++ dream/src/renderer/OpenGLRenderer.cpp
@@ -102,16 +102,23 @@ namespace Dream {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, (int) SHADOW_WIDTH, (int) SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        // TODO: webgl does not support GL_CLAMP_TO_BORDER...
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        #ifndef EMSCRIPTEN
         float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
         glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+        #endif
         // attach depth texture as FBO's depth buffer
         glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
-        // TODO: this causes issue where shadow is repeated in scene for web build...
         #ifndef EMSCRIPTEN
         glDrawBuffer(GL_NONE);
+        #else
+//        GLuint attachments[1] = { GL_NONE };
+//        glDrawBuffers(1, attachments);
         #endif
         glReadBuffer(GL_NONE);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
