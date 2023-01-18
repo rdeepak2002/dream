06f02725e2acd5573b4b0b81f6638d9acfe8f98d
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-01-17 21:19:43 -0500

    Almost done fixing shadows in web build - problem where shadows are repeated are present in web build only (most likely due to "glDrawBuffer(GL_NONE)" not being present.

--- dream/src/renderer/OpenGLRenderer.cpp
+++ dream/src/renderer/OpenGLRenderer.cpp
@@ -99,7 +99,7 @@ namespace Dream {
         // create depth texture
         glGenTextures(1, &depthMap);
         glBindTexture(GL_TEXTURE_2D, depthMap);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, (int) SHADOW_WIDTH, (int) SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
@@ -109,7 +109,10 @@ namespace Dream {
         // attach depth texture as FBO's depth buffer
         glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+        // TODO: this causes issue where shadow is repeated in scene for web build...
+        #ifndef EMSCRIPTEN
         glDrawBuffer(GL_NONE);
+        #endif
         glReadBuffer(GL_NONE);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
