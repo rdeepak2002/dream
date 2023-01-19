3e3bf15afd3446ef5bb662e6e89c3044cbe86050
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-01-19 11:35:08 -0500

    Use QuickJS instead of duktape

--- dream/src/main.cpp
+++ dream/src/main.cpp
@@ -18,7 +18,82 @@
 
 #include "dream/Application.h"
 #include <iostream>
-#include <duktape.h>
+#include <quickjs.h>
+#include <quickjs-libc.h>
+
+static JSValue js_print(JSContext *ctx, JSValueConst this_val,
+                        int argc, JSValueConst *argv)
+{
+    int i;
+    const char *str;
+
+    for(i = 0; i < argc; i++) {
+        if (i != 0)
+            putchar(' ');
+        str = JS_ToCString(ctx, argv[i]);
+        if (!str)
+            return JS_EXCEPTION;
+        fputs(str, stdout);
+        JS_FreeCString(ctx, str);
+    }
+    putchar('\n');
+    return JS_UNDEFINED;
+}
+
+void test_quickjs() {
+    JSRuntime* rt; JSContext* ctx;
+
+    rt = JS_NewRuntime();
+    ctx = JS_NewContextRaw(rt);
+
+    JS_AddIntrinsicBaseObjects(ctx);
+    JS_AddIntrinsicEval(ctx);
+
+    JSValue global_obj, console;
+
+    global_obj = JS_GetGlobalObject(ctx);
+
+    console = JS_NewObject(ctx);
+    JS_SetPropertyStr(ctx, console, "log",
+                      JS_NewCFunction(ctx, js_print, "log", 1));
+    JS_SetPropertyStr(ctx, global_obj, "console", console);
+
+    JS_FreeValue(ctx, global_obj);
+
+//    const char source[] = "console.log(Math.pow(12, 2));100";
+    auto source = "console.log(Math.pow(12, 2));";
+    size_t len = sizeof(source);
+
+    printf("Evaluate: %s length: %i\n", source, len);
+
+    JSValue ret = JS_Eval(ctx,
+                          source,
+                          len,
+                          "<evalScript>",
+                          JS_EVAL_TYPE_GLOBAL);
+
+    if (JS_IsException(ret)) {
+        js_std_dump_error(ctx);
+        JS_ResetUncatchableError(ctx);
+    }
+
+    JSValue d_JS = __JS_NewFloat64(ctx, 12.5);
+
+    int x = JS_VALUE_GET_INT(ret);
+
+    printf("Int: %i \n", x);
+    printf("Double: %f \n", JS_VALUE_GET_FLOAT64(d_JS));
+
+    JSValue i_JS = JS_NewInt32(ctx, 1);
+    JS_ToInt32(ctx, &x, i_JS);
+
+    printf("1 Int: %i \n", x);
+
+    JS_FreeContext(ctx);
+    JS_FreeRuntime(rt);
+
+    printf("Hello, C!\n");
+}
 
 #ifndef EMSCRIPTEN
 
@@ -58,10 +133,7 @@ void startUpdateLoop() {
 }
 
 int main(int ArgCount, char **Args) {
-    duk_context *ctx = duk_create_heap_default();
-    duk_eval_string(ctx, "1+2");
-    printf("1+2=%d\n", (int) duk_get_int(ctx, -1));
-    duk_destroy_heap(ctx);
+    test_quickjs();
 
     if (ArgCount >= 2 && std::string(Args[1]) == "test") {
 #ifndef EMSCRIPTEN
